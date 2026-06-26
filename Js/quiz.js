2c4afb646972029cbd67cd9720e8369f38ede885
dilan <medinadilan07@gmail.com>
2026-06-26 18:55:35 -0400

    termino de accion ocultar al seleccionar un tipos de quiz

--- Js/quiz.js
+++ Js/quiz.js
@@ -1,150 +1,159 @@
-const seccion_quiz= document.getElementById("seccion-quiz");
+// Seleccionamos todas tus tarjetas por su clase real
+const areas = document.querySelectorAll(".seccion-categoria");
 
-seccion_quiz.addEventListener("click",()=>{
-    //aca de deberiamos borrar o oculat la seccion de eleccion de quiz(mate,historia,informatica)
+areas.forEach(area => {
+    area.addEventListener('click', function(evento) {
 
-    //casos de lo que seleccione el usuario
-    const area_seleccionada = "matematica"; //ejemplo(matematica(deberia ir una variable))
+        // 2. Buscamos el contenedor principal usando .closest()
+        const padrePrincipal = this.closest("#menu-seleccion"); 
+        padrePrincipal.classList.add("ocultar_categorias_quiz");
 
-    switch (area_seleccionada) {
-    case "informatica":
-        console.log("entrada a test informatica");
+    });
+});
 
-        quiz_informatica();
+// seccion_quiz.addEventListener("click",()=>{
+//     //aca de deberiamos borrar o oculat la seccion de eleccion de quiz(mate,historia,informatica)
+
+//     //casos de lo que seleccione el usuario
+//     const area_seleccionada = "matematica"; //ejemplo(matematica(deberia ir una variable))
+
+//     switch (area_seleccionada) {
+//     case "informatica":
+//         console.log("entrada a test informatica");
+
+//         quiz_informatica();
 
         
-        break;
-    case "ciencia":
-        console.log("Puedes editar contenido.");
-        break;
-    case "peliculas":
-        console.log("Acceso total al sistema.");
-        break;
-    case "geografia":
-        console.log("Acceso total al sistema.");
-        break;
-    case "musica":
-        console.log("Acceso total al sistema.");
-        break;
-    case "deporte":
-        console.log("Acceso total al sistema.");
-        break;
+//         break;
+//     case "ciencia":
+//         console.log("A");
+//         break;
+//     case "peliculas":
+//         console.log("A");
+//         break;
+//     case "geografia":
+//         console.log("A");
+//         break;
+//     case "musica":
+//         console.log("A");
+//         break;
+//     case "deporte":
+//         console.log("A");
+//         break;
 
 
-    default:
-        console.log("Rol no reconocido.");
-    }
+//     default:
+//         console.log("Rol no reconocido.");
+//     }
 
-})
+// })
 
-async function quiz_informatica(facil){
+// async function quiz_informatica(facil){
 
-    //llmada a la api y verificar que tipo se seleccion hizo el usurio (facil,medio, avanzado)
-    //en el fech poner una variable de la api si es facil o hard.
+//     //llmada a la api y verificar que tipo se seleccion hizo el usurio (facil,medio, avanzado)
+//     //en el fech poner una variable de la api si es facil o hard.
 
-    try {
-    const api_quiz = await fetch("https://opentdb.com/api.php?amount=10&category=18&difficulty=easy&type=multiple&encode=url3986");
-    if(!api_quiz.ok){
-            throw new Error("Hubo un error en la peticion de la api");
-    }
+//     try {
+//     const api_quiz = await fetch("https://opentdb.com/api.php?amount=10&category=18&difficulty=easy&type=multiple&encode=url3986");
+//     if(!api_quiz.ok){
+//             throw new Error("Hubo un error en la peticion de la api");
+//     }
 
-    const ontenido_json = await datos.json();
-    console.log("datos de la api: ", contenido_json);
+//     const ontenido_json = await datos.json();
+//     console.log("datos de la api: ", contenido_json);
 
-    const preguntas = contenido_json.results;
+//     const preguntas = contenido_json.results;
     
-    console.log("********************");
-    console.log("Traduciendo el array de preguntas... Por favor espera.");
+//     console.log("********************");
+//     console.log("Traduciendo el array de preguntas... Por favor espera.");
 
-    // Variable donde se guardará todo el array traducido
-    const preguntasTraducidas = [];
+//     // Variable donde se guardará todo el array traducido
+//     const preguntasTraducidas = [];
 
-    for (let i = 0; i < preguntas.length; i++) {
-        // 1. Decodificar y traducir la PREGUNTA
-        const preguntaIngles = decodeURIComponent(preguntas[i].question);
-        const urlProg = `https://api.mymemory.translated.net/get?q=${encodeURIComponent(preguntaIngles)}&langpair=en|es`;
-        const resPreg = await fetch(urlProg);
-        const datosPreg = await resPreg.json();
-        const preguntaEspanol = datosPreg.responseData.translatedText;
+//     for (let i = 0; i < preguntas.length; i++) {
+//         // 1. Decodificar y traducir la PREGUNTA
+//         const preguntaIngles = decodeURIComponent(preguntas[i].question);
+//         const urlProg = `https://api.mymemory.translated.net/get?q=${encodeURIComponent(preguntaIngles)}&langpair=en|es`;
+//         const resPreg = await fetch(urlProg);
+//         const datosPreg = await resPreg.json();
+//         const preguntaEspanol = datosPreg.responseData.translatedText;
 
-        // 2. Decodificar y traducir la RESPUESTA CORRECTA
-        const correctaIngles = decodeURIComponent(preguntas[i].correct_answer);
-        const urlCorr = `https://api.mymemory.translated.net/get?q=${encodeURIComponent(correctaIngles)}&langpair=en|es`;
-        const resCorr = await fetch(urlCorr);
-        const datosCorr = await resCorr.json();
-        const correctaEspanol = datosCorr.responseData.translatedText;
+//         // 2. Decodificar y traducir la RESPUESTA CORRECTA
+//         const correctaIngles = decodeURIComponent(preguntas[i].correct_answer);
+//         const urlCorr = `https://api.mymemory.translated.net/get?q=${encodeURIComponent(correctaIngles)}&langpair=en|es`;
+//         const resCorr = await fetch(urlCorr);
+//         const datosCorr = await resCorr.json();
+//         const correctaEspanol = datosCorr.responseData.translatedText;
 
-        // 3. Decodificar y traducir las RESPUESTAS INCORRECTAS (es un array)
-        const incorrectasEspanol = [];
-        for (let j = 0; j < preguntas[i].incorrect_answers.length; j++) {
-            const incIngles = decodeURIComponent(preguntas[i].incorrect_answers[j]);
-            const urlInc = `https://api.mymemory.translated.net/get?q=${encodeURIComponent(incIngles)}&langpair=en|es`;
-            const resInc = await fetch(urlInc);
-            const datosInc = await resInc.json();
-            incorrectasEspanol.push(datosInc.responseData.translatedText);
-        }
+//         // 3. Decodificar y traducir las RESPUESTAS INCORRECTAS (es un array)
+//         const incorrectasEspanol = [];
+//         for (let j = 0; j < preguntas[i].incorrect_answers.length; j++) {
+//             const incIngles = decodeURIComponent(preguntas[i].incorrect_answers[j]);
+//             const urlInc = `https://api.mymemory.translated.net/get?q=${encodeURIComponent(incIngles)}&langpair=en|es`;
+//             const resInc = await fetch(urlInc);
+//             const datosInc = await resInc.json();
+//             incorrectasEspanol.push(datosInc.responseData.translatedText);
+//         }
 
-        // 4. Guardar el objeto estructurado en nuestro nuevo array
-        preguntasTraducidas.push({
-            categoria: decodeURIComponent(preguntas[i].category),
-            tipo: decodeURIComponent(preguntas[i].type),
-            dificultad: decodeURIComponent(preguntas[i].difficulty),
-            pregunta: preguntaEspanol,
-            respuesta_correcta: correctaEspanol,
-            respuestas_incorrectas: incorrectasEspanol
-        });
+//         // 4. Guardar el objeto estructurado en nuestro nuevo array
+//         preguntasTraducidas.push({
+//             categoria: decodeURIComponent(preguntas[i].category),
+//             tipo: decodeURIComponent(preguntas[i].type),
+//             dificultad: decodeURIComponent(preguntas[i].difficulty),
+//             pregunta: preguntaEspanol,
+//             respuesta_correcta: correctaEspanol,
+//             respuestas_incorrectas: incorrectasEspanol
+//         });
 
-        console.log(`Progreso: ${i + 1}/10 traducidas.`);
-    }
+//         console.log(`Progreso: ${i + 1}/10 traducidas.`);
+//     }
 
-    // Aquí ya tienes todo guardado en la variable list para usar en tu app
-    console.log("********************");
-    console.log("¡Traducción completada con éxito!");
-    console.log("Variable 'preguntasTraducidas':", preguntasTraducidas);
-
-
-    //preguntastraducidas contiene todo la array obtenida por la api open triva pero traducida
-
-    //foreach la mostrar el contenido del primer elemtro de la array y luego esperar una respuesta de los botones del quiz
-
-    preguntasTraducidas.forEach(dato=>{
-        seccion_quiz.innerHTML +=`
-
-        <picture>    <!--imagen-->
-            <img src="/Img/Banner Proyecto Quiz Interactivo.png" class="img-fluid img-banner" alt="Banner de QuizMind">
-        </picture>
-
-        <div class="row">
-            <h4 class="category-title">${dato.title}</h4> <!--configuar con js y css-->
-        </div>
-        <div class="row">
-            <h3 class="question-title">¿Cuál es la unidad básica de información en la computación clásica que puede representar un estado de 0 o 1?</h3> <!--configurar con js y css-->
-        </div>
+//     // Aquí ya tienes todo guardado en la variable list para usar en tu app
+//     console.log("********************");
+//     console.log("¡Traducción completada con éxito!");
+//     console.log("Variable 'preguntasTraducidas':", preguntasTraducidas);
 
 
-        <div class="btn-group-horizontal" role="group" aria-label="Horizontal button group">
-            <button type="button" class="btn btn-primary alternativa-a">A. Byte</button>
-            <button type="button" class="btn btn-primary alternativa-b">B. Bit</button>
-        </div>
-        <div class="btn-group-horizontal" role="group" aria-label="Horizontal button group">
-            <button type="button" class="btn btn-primary alternativa-c">C. Pixel</button>
-            <button type="button" class="btn btn-primary alternativa-d">D. Hercio</button>
-        </div>
-        <!--botones de saltar y siguiente-->
-        <div class="btn-group-horizontal" role="group" aria-label="Horizontal button group">
-            <button type="button" class="btn btn-primary saltar">Saltar</button>
-            <button type="button" class="btn btn-primary siguiente">Siguiente</button>
-        </div>
+//     //preguntastraducidas contiene todo la array obtenida por la api open triva pero traducida
+
+//     //foreach la mostrar el contenido del primer elemtro de la array y luego esperar una respuesta de los botones del quiz
+
+//     preguntasTraducidas.forEach(dato=>{
+//         seccion_quiz.innerHTML +=`
+
+//         <picture>    <!--imagen-->
+//             <img src="/Img/Banner Proyecto Quiz Interactivo.png" class="img-fluid img-banner" alt="Banner de QuizMind">
+//         </picture>
+
+//         <div class="row">
+//             <h4 class="category-title">${dato.title}</h4> <!--configuar con js y css-->
+//         </div>
+//         <div class="row">
+//             <h3 class="question-title">¿Cuál es la unidad básica de información en la computación clásica que puede representar un estado de 0 o 1?</h3> <!--configurar con js y css-->
+//         </div>
+
+
+//         <div class="btn-group-horizontal" role="group" aria-label="Horizontal button group">
+//             <button type="button" class="btn btn-primary alternativa-a">A. Byte</button>
+//             <button type="button" class="btn btn-primary alternativa-b">B. Bit</button>
+//         </div>
+//         <div class="btn-group-horizontal" role="group" aria-label="Horizontal button group">
+//             <button type="button" class="btn btn-primary alternativa-c">C. Pixel</button>
+//             <button type="button" class="btn btn-primary alternativa-d">D. Hercio</button>
+//         </div>
+//         <!--botones de saltar y siguiente-->
+//         <div class="btn-group-horizontal" role="group" aria-label="Horizontal button group">
+//             <button type="button" class="btn btn-primary saltar">Saltar</button>
+//             <button type="button" class="btn btn-primary siguiente">Siguiente</button>
+//         </div>
         
-        `;
+//         `;
 
-    })
+//     })
 
-    } catch (e) {
-        console.log("Error en el proceso: ", e);
-    }
-
-
+//     } catch (e) {
+//         console.log("Error en el proceso: ", e);
+//     }
 
 
 
@@ -152,7 +161,9 @@ async function quiz_informatica(facil){
 
 
 
-}
+
+
+// }
 
 
 
